10d79ae8c6339f80673a74a289788b13ca294601
Rania Haj <haj.rania@hotmail.com>
2023-02-25 22:44:02 +0200

    project

---
+++ main.js
@@ -0,0 +1,52 @@
+const displayUser = function () {
+  let apiManager = new APIManager();
+  let renderer = new Renderer();
+
+  apiManager.displayData
+    .getRandomUsers()
+    .then((data) => {
+      renderer.GenericTemplateGenerator(
+        `.user-container`,
+        `#userInfo`,
+        data.results[0]
+      );
+      data.results.splice(0, 1);
+      return data.results;
+    })
+    .then((data) => {
+      renderer.GenericTemplateGenerator(
+        `.friends-container`,
+        `#userFriendsNameList`,
+        data
+      );
+    })
+    .then(() => {
+      apiManager.displayData.getRandomQuote().then((data) => {
+        renderer.GenericTemplateGenerator(
+          `.quote-container`,
+          `#kanyeQuote`,
+          data
+        );
+      });
+    })
+    .then(() => {
+      apiManager.displayData.getRandomPokemon().then((data) => {
+        renderer.GenericTemplateGenerator(
+          `.pokemon-container`,
+          `#pokemonInfo`,
+          data
+        );
+      });
+    })
+    .then(() => {
+      apiManager.displayData.getRandomBacon().then((data) => {
+        renderer.GenericTemplateGenerator(
+          `.meat-container`,
+          `#baconParagraph`,
+          data
+        );
+      });
+    });
+};
+
+displayUser();
